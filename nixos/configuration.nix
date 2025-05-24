7319a7877b11a935291a875f5cc06076c5f9a9b6
DVogel <dvogelca@proton.me>
2025-05-24 08:32:51 -0400

    feat(system): add nvidia configs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -128,15 +128,30 @@
   time.timeZone = "America/Detroit";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
+  # Enable the GNOME Desktop Environment.
   services.xserver = {
     enable = true;
+    videoDrivers = ["nvidia"];
     xkb.layout = "us";
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = true;
+    desktopManager.gnome.enable = true;
   };
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Nvidia
+  hardware.nvidia = {
+    modesetting.enable = true;
+    forceFullCompositionPipeline = true;
+    powerManagement.enable = true;
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # Force GLX to use NVIDIA
+  };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
