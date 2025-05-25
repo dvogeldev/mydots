205f4c71b5d466e8742c5e3cf1854acf3e5dc578
DVogel <dvogelca@proton.me>
2025-05-25 17:17:20 -0400

    feat(flatpak): add repo via script

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -148,6 +148,13 @@
 
   # Flatpaks
   services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [pkgs.flatpak];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
   # services.libinput.enable = true;
 
