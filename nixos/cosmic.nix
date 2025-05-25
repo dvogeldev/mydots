e858a88ba35e1adea2444d6ad747b795e16bbb6b
DVogel <dvogelca@proton.me>
2025-05-25 08:01:43 -0400

    feat(system): add xwayland support for cosmic desktop

--- nixos/cosmic.nix
+++ nixos/cosmic.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
-  services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic = {
+    enable = true;
+    xwayland.enable = true;
+  };
   services.displayManager.cosmic-greeter.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
 }
