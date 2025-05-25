ee0063efba0b9358c38f612939fe33b8873deb64
DVogel <dvogelca@proton.me>
2025-05-25 07:36:32 -0400

    fix(user): add podman group

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -164,7 +164,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel" "networkmanager" "libvirtd"];
+      extraGroups = ["wheel" "networkmanager" "libvirtd" "podman"];
     };
   };
 
